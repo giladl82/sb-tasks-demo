b78f2093702f070f5115e9dace167740850b1429
Gilad Lev-Ari <giladl82@gmail.com>
2025-01-24 11:42:38 +0200

    chore: Add  to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,16 @@
+import { useEffect } from "react";
+
 function App() {
+  useEffect(() => {
+    const test = async () => {
+      const response = await fetch('http://localhost:3000/tasks')
+      const data = await response.json();
+      console.log(data);
+    }
+
+    test();
+  }, [])
+
   return <h1 className="p-4 text-3xl text-blue-600">This is a title</h1>;
 }
 
