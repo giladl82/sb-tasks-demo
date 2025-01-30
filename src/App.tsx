c03f5d36825868223ddad22fec714ab3446767ba
Gilad Lev-Ari <giladl82@gmail.com>
2025-01-30 14:41:46 +0200

    chore: Add react-query devtools

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Tasks } from './pages/Tasks';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const queryClient = new QueryClient();
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <Tasks />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
