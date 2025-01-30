dc550822a9be85c92234588d77f3adcb083522d2
Gilad Lev-Ari <giladl82@gmail.com>
2025-01-30 12:54:57 +0200

    chore: Add more alias

--- vite.config.ts
+++ vite.config.ts
@@ -8,8 +8,11 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
+      '@assets': path.resolve(__dirname, 'src/assets'),
       '@components': path.resolve(__dirname, 'src/components'),
       '@models': path.resolve(__dirname, 'src/models'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
+      '@services': path.resolve(__dirname, 'src/services'),
     } as AliasOptions,
   },
 });
